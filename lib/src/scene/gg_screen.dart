9eaa927082364f8c952e0b171733cc2fe0f6322b
paulw <paulw@neara.com>
2022-10-01 20:58:37 +1000

    Add gg screen text message

--- lib/src/scene/gg_screen.dart
+++ lib/src/scene/gg_screen.dart
@@ -20,6 +20,10 @@ class GameOverScreen {
     r.ctx.beginPath();
     r.ctx.setFillColorRgb(255, 0, 0);
     r.ctx.rect(0,0, r.width, r.height);
+
+    r.ctx.strokeText("GAME OVER. Click to try again", r.width / 2, r.height / 2);
+    r.ctx.textAlign = "center";
+
     r.ctx.stroke();
   }
 }
