3c9a3aab2c56bb6df18f9fa2c95bf3bf456fbc5c
paulw <paulw@neara.com>
2022-10-01 16:03:22 +1000

    Add beginPath before drawing

--- lib/src/render/renderer.dart
+++ lib/src/render/renderer.dart
@@ -10,13 +10,15 @@ class Renderer {
   num get width => canvas.width!;
   num get height => canvas.height!;
 
-  Renderer(this.canvas) : ctx = canvas.context2D;
+  Renderer(this.canvas) : ctx = canvas.context2D {
+  }
 
   void beginRender() {
     ctx.clearRect(0, 0, width, height);
   }
 
   void renderCircle(Vector2 v) {
+    ctx.beginPath();
     ctx.arc(v.x, v.y, 10.0, 0.0, 2 * pi);
     ctx.stroke();
   }
